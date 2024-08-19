9f477f7ad989bfbff5e51f43307279ce2234ca1d
gokulrajgokul <gokul26.cbcs@gmail.com>
2024-08-19 16:55:00 +0530

    added

---
+++ project/main.py
@@ -0,0 +1,26 @@
+# pip install gunicorn - host a project in website
+# pip freeze > requirement.txt - packages in hosting
+
+
+import pickle
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+
+# url/
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['GET', 'POST'])
+def predict():
+    prediction = model.predict([[float(request.form.get("temperature"))]])
+    output = round(prediction[0],2)
+    return render_template('index.html',  prediction_text=f'Total revenve generated is Rs. {output}/-')
+
+if __name__=='__main__':
+    app.run(debug=True)
